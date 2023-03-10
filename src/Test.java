58a5f6a508c9706227ef0ba053fa66cd3cc79010
lqdu <lqdu>
2023-03-10 14:15:32 +0800

    init

---
+++ src/Test.java
@@ -0,0 +1,17 @@
+public class Test {
+    void show(int i){
+        if(i==1){
+            System.out.println(1);
+        }else if(i==2){
+            System.out.println(2);
+        }else if(i==3){
+            System.out.println(3);
+        }else if(i==4){
+            System.out.println(4);
+        }else if(i==5){
+            System.out.println(5);
+        }else if(i==6){
+            System.out.println(6);
+        }
+    }
+}
